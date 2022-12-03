d9b959bf5c52ce6e7bbf7ba248217e26766c3b64
A2 <114377691+aaboodza@users.noreply.github.com>
2022-12-03 01:53:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,33 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+var url = "http://localhost:3000";  
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Homework Heroes');
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
-
-// Accordion
-function myFunction(id) {
-  var x = document.getElementById(id);
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-    x.previousElementSibling.className += " w3-theme-d1";
-  } else { 
-    x.className = x.className.replace("w3-show", "");
-    x.previousElementSibling.className = 
-    x.previousElementSibling.className.replace(" w3-theme-d1", "");
-  }
-}
-
-// Used to toggle the menu on smaller screens when clicking on the menu button
-function openNav() {
-  var x = document.getElementById("navDemo");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else { 
-    x.className = x.className.replace(" w3-show", "");
-  }
-}
