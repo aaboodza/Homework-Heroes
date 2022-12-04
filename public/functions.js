cf1b4a8cddc84c2bbc43bd56a04d2b48fb09ac3a
A2 <114377691+aaboodza@users.noreply.github.com>
2022-12-03 22:10:08 -0500

    Update functions.js

--- public/functions.js
+++ public/functions.js
@@ -8,13 +8,17 @@ function getCheckedValue(radioName) {
 		if (radios[y].checked) return radios[y].value;
 }
 
+// Calendar function
 function dateAndTime() {
 	outputObj = document.getElementById("date");
-	var input = parseInt(prompt("What day of the month is your assignment due?: "));
+	outputObj2 = document.getElementById("taskTitle");
+	var input2 = prompt("Task title?");
+	var input = parseInt(prompt("What day of the month is your assignment due? (input as number) "));
     	const d = new Date();
     	var dayMonth = d.getDate();
     	const newDay = (input - dayMonth);
-	outputObj.innerHTML= "Your assignment is due " + newDay + " days from now.";
+	outputObj.innerHTML= newDay + " day(s)";
+	outputObj2.innerHTML= "Task : " + input2;
 }
 
 function getScore() {
@@ -32,9 +36,10 @@ function returnScore() {
 	}
 }
 
+
 //Convert grade book to PDF
 function convertToPDF() {
-	var report = document.getElementById('score1').innerHTML;
+	var report = document.getElementById('transcript').innerHTML;
 	var win = window.open('', '', 'height=700,width=700');
 	win.document.write('<title>Grade Report</title>');   
 	win.document.write(report);         
@@ -64,4 +69,42 @@ function openNav() {
 	} else {
 		x.className = x.className.replace(" w3-show", "");
 	}
+}
+
+//Edit name
+function editName() {
+	outputObj1 = document.getElementById("name1");
+	outputObj2 = document.getElementById("name2");
+	var input1 = prompt("Please Enter your full name : ");
+	var input2 = prompt("And where do you study?");
+	outputObj1.innerHTML= '<i class="fa fa-user fa-fw w3-margin-right w3-text-theme"></i>' + "Name : " + input1;
+	outputObj2.innerHTML= '<i class="fa fa-graduation-cap fa-fw w3-margin-right w3-text-theme"></i>' + "University : " + input2;
+
+}
+
+//Progress bars
+function htmlBar() {
+    document.getElementById("htmlBartext").style.width="50%"; 
+	outputObj = document.getElementById("htmlBartext");
+	outputObj.innerHTML= "50%";
+}
+
+function cssBar() {
+    document.getElementById("cssBartext").style.width="50%"; 
+	outputObj = document.getElementById("cssBartext");
+	outputObj.innerHTML= "50%";
+}
+
+function barComp() {
+    document.getElementById("htmlBartext").style.width="100%"; 
+	outputObj = document.getElementById("htmlBartext");
+	outputObj.innerHTML= "100%";
+
+	document.getElementById("cssBartext").style.width="100%"; 
+	outputObj2 = document.getElementById("cssBartext");
+	outputObj2.innerHTML= "100%";
+
+	document.getElementById("jsBartext").style.width="100%"; 
+	outputObj3 = document.getElementById("jsBartext");
+	outputObj3.innerHTML= "100%";
 }
