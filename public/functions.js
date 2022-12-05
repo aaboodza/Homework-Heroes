6af28f542d953a4eae6550bfbf1bde7d17238e77
A2 <114377691+aaboodza@users.noreply.github.com>
2022-12-04 20:41:18 -0500

    Update functions.js

--- public/functions.js
+++ public/functions.js
@@ -1,11 +1,12 @@
-//Test and Grade system
-var answers = ["A", "C", "B"],
-	tot = answers.length;
+//Edit name
+function editName() {
+	outputObj1 = document.getElementById("name1");
+	outputObj2 = document.getElementById("name2");
+	var input1 = prompt("Please Enter your full name : ");
+	var input2 = prompt("And where do you study?");
+	outputObj1.innerHTML = '<i class="fa fa-user fa-fw w3-margin-right w3-text-theme"></i>' + "Name : " + input1;
+	outputObj2.innerHTML = '<i class="fa fa-graduation-cap fa-fw w3-margin-right w3-text-theme"></i>' + "University : " + input2;
 
-function getCheckedValue(radioName) {
-	var radios = document.getElementsByName(radioName);
-	for (var y = 0; y < radios.length; y++)
-		if (radios[y].checked) return radios[y].value;
 }
 
 // Calendar function
@@ -14,11 +15,42 @@ function dateAndTime() {
 	outputObj2 = document.getElementById("taskTitle");
 	var input2 = prompt("Task title?");
 	var input = parseInt(prompt("What day of the month is your assignment due? (input as number) "));
-    	const d = new Date();
-    	var dayMonth = d.getDate();
-    	const newDay = (input - dayMonth);
-	outputObj.innerHTML= newDay + " day(s)";
-	outputObj2.innerHTML= "Task : " + input2;
+	const d = new Date();
+	var dayMonth = d.getDate();
+	const newDay = (input - dayMonth);
+	outputObj.innerHTML = newDay + " day(s)";
+	outputObj2.innerHTML = "Task : " + input2;
+}
+
+//Progress bars
+function htmlBar() {
+	document.getElementById("htmlBartext").style.width = "50%";
+	outputObj = document.getElementById("htmlBartext");
+	outputObj.innerHTML = "50%";
+}
+
+function cssBar() {
+	document.getElementById("cssBartext").style.width = "50%";
+	outputObj = document.getElementById("cssBartext");
+	outputObj.innerHTML = "50%";
+}
+
+//showing Right Answers to the test
+function ansWers() {
+	document.getElementById("answer1").style.visibility = "visible";
+	document.getElementById("answer2").style.visibility = "visible";
+	document.getElementById("answer3").style.visibility = "visible";
+
+}
+
+//Test and Grade system
+var answers = ["A", "C", "B"],
+	tot = answers.length;
+
+function getCheckedValue(radioName) {
+	var radios = document.getElementsByName(radioName);
+	for (var y = 0; y < radios.length; y++)
+		if (radios[y].checked) return radios[y].value;
 }
 
 //geting the grade from the test
@@ -38,17 +70,32 @@ function returnScore() {
 	}
 }
 
-
 //Convert grade book to PDF
 function convertToPDF() {
 	var report = document.getElementById('transcript').innerHTML;
 	var win = window.open('', '', 'height=700,width=700');
-	win.document.write('<title>Grade Report</title>');   
-	win.document.write(report);         
+	win.document.write('<title>Grade Report</title>');
+	win.document.write(report);
 
-	win.document.close(); 	
-	win.print();    
+	win.document.close();
+	win.print();
 }
+
+//Function for completing ALL progress bars
+function barComp() {
+	document.getElementById("htmlBartext").style.width = "100%";
+	outputObj = document.getElementById("htmlBartext");
+	outputObj.innerHTML = "100%";
+
+	document.getElementById("cssBartext").style.width = "100%";
+	outputObj2 = document.getElementById("cssBartext");
+	outputObj2.innerHTML = "100%";
+
+	document.getElementById("jsBartext").style.width = "100%";
+	outputObj3 = document.getElementById("jsBartext");
+	outputObj3.innerHTML = "100%";
+}
+
 
 // Accordion
 function myFunction(id) {
@@ -71,43 +118,4 @@ function openNav() {
 	} else {
 		x.className = x.className.replace(" w3-show", "");
 	}
-}
-
-//Edit name
-function editName() {
-	outputObj1 = document.getElementById("name1");
-	outputObj2 = document.getElementById("name2");
-	var input1 = prompt("Please Enter your full name : ");
-	var input2 = prompt("And where do you study?");
-	outputObj1.innerHTML= '<i class="fa fa-user fa-fw w3-margin-right w3-text-theme"></i>' + "Name : " + input1;
-	outputObj2.innerHTML= '<i class="fa fa-graduation-cap fa-fw w3-margin-right w3-text-theme"></i>' + "University : " + input2;
-
-}
-
-//Progress bars
-function htmlBar() {
-    document.getElementById("htmlBartext").style.width="50%"; 
-	outputObj = document.getElementById("htmlBartext");
-	outputObj.innerHTML= "50%";
-}
-
-function cssBar() {
-    document.getElementById("cssBartext").style.width="50%"; 
-	outputObj = document.getElementById("cssBartext");
-	outputObj.innerHTML= "50%";
-}
-
-//Function for completing ALL progress bars
-function barComp() {
-    document.getElementById("htmlBartext").style.width="100%"; 
-	outputObj = document.getElementById("htmlBartext");
-	outputObj.innerHTML= "100%";
-
-	document.getElementById("cssBartext").style.width="100%"; 
-	outputObj2 = document.getElementById("cssBartext");
-	outputObj2.innerHTML= "100%";
-
-	document.getElementById("jsBartext").style.width="100%"; 
-	outputObj3 = document.getElementById("jsBartext");
-	outputObj3.innerHTML= "100%";
 }
